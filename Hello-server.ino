a69e4f2ebec2c26bb51bd916daa8ca5163295e5a
zungzeny808 <zungzeny808@gmail.com>
2016-01-31 20:37:40 +0700

    Update Hello-server.ino

--- Hello-server.ino
+++ Hello-server.ino
@@ -3,8 +3,8 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
-const char* ssid = "Wilddesk";
-const char* password = "whitelegbluedress";
+const char* ssid = "xxxx";
+const char* password = "xxxx";
 
 ESP8266WebServer server(80);
 #define LED_ON 0
